d51265fe7e22c228b7ac53a138a543489753068c
pianeta05 <pianeta05@hotmail.com>
2021-08-18 16:50:34 +0100

    adding event to pageLinks

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const links = document.querySelectorAll('[data-to]');
 const divContents = document.getElementsByClassName('content');
+const pageLinks = document.querySelectorAll('[data-page]');
+const pageContents = document.getElementsByClassName('list-page');
 
 for (const link of links) {
     link.addEventListener('click', ({ target }) => {
@@ -16,5 +18,23 @@ for (const link of links) {
         }
         target.classList.add('active');
         // Modificar la URL
+    });
+}
+
+// Evento click de las páginas
+for (const pageLink of pageLinks) {
+    pageLink.addEventListener('click', () => {
+        const page = pageLink.dataset.page;
+        for (const pageContent of pageContents) {
+            if (pageContent.id === `page-${page}`) {
+                pageContent.classList.add('active');
+            } else {
+                pageContent.classList.remove('active');
+            }
+        }
+        for (const link of pageLinks) {
+            link.parentElement.classList.remove('active');
+        }
+        pageLink.parentElement.classList.add('active');
     });
 }
